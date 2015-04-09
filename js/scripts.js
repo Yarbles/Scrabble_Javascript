9403d74d2e5c0a1de391047dd90ace6e16132811
Liz Beacham & Reid Baldwin <student@epicodus.com>
2015-04-09 11:59:16 -0700

    add two letter word functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,28 @@
 var scrabbleScore = function(input) {
     var score = [];
-    // var input_split = [];
-
     var input_split = input.split("");
-
-    // var letters = input_split.push(split_it);
+    console.log(input_split);
 
     var one = ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"];
     var two = ["D", "G"];
 
     var tally = 0;
+    console.log(tally);
+
     var final_score = 0;
 
     for (var i=0; i< input_split.length; i++) {
         if($.inArray(input_split[i], one) !== -1) {
             tally++;
+            console.log(tally);
             score.push(tally);
             tally = 0;
+            console.log(tally);
         }
-        $.each(score,function() {
-            final_score += this;
-        });
     }
+    $.each(score,function() {
+        final_score += this;
+        console.log(final_score);
+    });
     return final_score;
 };
--- specs/specs.js
+++ specs/specs.js
@@ -2,4 +2,8 @@ describe('scrabbleScore', function() {
     it("will return a score of 1 when A is inputted", function() {
      expect(scrabbleScore("A")).to.equal(1);
     });
+
+    it("returns a score of 2 when two letters worth 1 pt each are inputted", function() {
+        expect(scrabbleScore("AT")).to.equal(2);
+    });
 });
