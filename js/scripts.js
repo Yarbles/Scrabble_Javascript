ad2f5bffd7201b2aff208e31f1f92b9d7ab2dabb
Liz Beacham & Reid Baldwin <student@epicodus.com>
2015-04-09 12:22:12 -0700

    Add passing tests for all logic in function, plus account for input in lower case.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,52 @@
 var scrabbleScore = function(input) {
     var score = [];
-    var input_split = input.split("");
-    console.log(input_split);
+    var input_split = input.toUpperCase().split("");
 
     var one = ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"];
     var two = ["D", "G"];
+    var three = ["B", "C", "M", "P"];
+    var four = ["F", "H", "V", "W", "Y"];
+    var five = ["K"];
+    var eight = ["J", "X"];
+    var ten = ["Q", "Z"];
 
     var tally = 0;
-    console.log(tally);
-
     var final_score = 0;
 
     for (var i=0; i< input_split.length; i++) {
         if($.inArray(input_split[i], one) !== -1) {
             tally++;
-            console.log(tally);
             score.push(tally);
             tally = 0;
-            console.log(tally);
-        }
+        } else if($.inArray(input_split[i], two) !== -1) {
+            tally+=2;
+            score.push(tally);
+            tally = 0;
+        } else if($.inArray(input_split[i], three) !== -1) {
+            tally+=3;
+            score.push(tally);
+            tally = 0;
+        }else if($.inArray(input_split[i], four) !== -1) {
+            tally+=4;
+            score.push(tally);
+            tally = 0;
+        }else if($.inArray(input_split[i], five) !== -1) {
+            tally+=5;
+            score.push(tally);
+            tally = 0;
+        }else if($.inArray(input_split[i], eight) !== -1) {
+            tally+=8;
+            score.push(tally);
+            tally = 0;
+        }else if($.inArray(input_split[i], ten) !== -1) {
+            tally+=10;
+            score.push(tally);
+            tally = 0;
+        };
     }
+
     $.each(score,function() {
         final_score += this;
-        console.log(final_score);
     });
     return final_score;
 };
--- specs/specs.js
+++ specs/specs.js
@@ -6,4 +6,21 @@ describe('scrabbleScore', function() {
     it("returns a score of 2 when two letters worth 1 pt each are inputted", function() {
         expect(scrabbleScore("AT")).to.equal(2);
     });
+
+    it("returns a score of 3 then a word with one 2-point letter and 1 1-point letter is inputted", function() {
+        expect(scrabbleScore("DO")).to.equal(3);
+    });
+
+    it("returns a score of 4 when a word with one 3-point letter and 1 1-point letter is inputted", function() {
+        expect(scrabbleScore("BE")).to.equal(4);
+    });
+
+    it("returns a score of 5 when a word with one 4-point letter and 1 1-point letter is inputted", function() {
+        expect(scrabbleScore("HE")).to.equal(5);
+    });
+
+    it("returns a score of 29 when a 8-pt, 1-pt, and 2 10-pt letters are inputted in lowercase", function() {
+        expect(scrabbleScore("jazz")).to.equal(29);
+    });
+
 });
